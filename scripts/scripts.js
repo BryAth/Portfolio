e1d8be011e4d07c9027b672abcd908c047c034b5
BryAth <lequeux.bryan95@gmail.com>
2022-06-18 11:40:42 +0200

    Micro update2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,12 @@
 let container2=document.querySelector(".container2");
 let topelement=document.querySelector(".para");
 let randomhihi=document.querySelector(".random")
+let javascriptlogo=document.querySelector(".javascriptlogo:hover")
 
 
 
 gsap.fromTo(container2, {opacity: 0}, {opacity: 1, duration: 1});
 gsap.fromTo(topelement, {opacity:0,x:-800},{opacity:1,x:0,duration:2});
-gsap.fromTo(randomhihi, {opacity:0,y:400},{opacity:1,y:0,duration:2});
+// gsap.fromTo(randomhihi, {opacity:0,y:400},{opacity:1,y:0,duration:2});
+
+gsap.to(javascriptlogo,{y:800})
